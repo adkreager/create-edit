914e595b22f68b5c0f654e9de7f2902b06c44f65
Alexis Kreager <alexisdkreager@gmail.com>
2020-08-10 08:28:03 -0500

    initial commit

---
+++ create-edit-spring/create-edit/src/main/java/com/example/createedit/CreateEditApplication.java
@@ -0,0 +1,13 @@
+package com.example.createedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateEditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateEditApplication.class, args);
+	}
+
+}
---
+++ create-edit-spring/create-edit/src/test/java/com/example/createedit/CreateEditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateEditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
